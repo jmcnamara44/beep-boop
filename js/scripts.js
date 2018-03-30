20007f489073ff59e0ee53b4ccec8a0a7734a306
Jimmy McNamara <jmcnamara44@gmail.com>
2018-03-30 14:21:30 -0700

    show the user an error message if they enter something other than an integer. passes the ninth listed behavior.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //business logic
 //user-interface logic
-var array = [];
+
 
 $(document).ready(function() {
   $("form#number-input").submit(function(event) {
@@ -9,20 +9,27 @@ $(document).ready(function() {
     var result = message(userInput);
 
     function message (input) {
-      array = [];
+      var array = [];
       var topNumber = input;
       var string = input.toString();
-      if (input % 3 === 0 && input !== 0) {
-        return "I'm sorry Dave. I'm afraid I can't do that";
-      } else if (string.indexOf(1) !== -1) {
-        return "BOOP!";
-      } else if (string.indexOf(0) !== -1) {
-        return "BEEP!";
+
+      if (input % 1 !== 0) {
+        return "Stop fooling around Dave.";
       } else {
-        for (var index = topNumber; index>-1; index--) {
-          array.push(index);
-        };
-        return array;
+
+        if (input % 3 === 0 && input !== 0) {
+          return "I'm sorry Dave. I'm afraid I can't do that";
+        } else if (string.indexOf(1) !== -1) {
+          return "BOOP!";
+        } else if (string.indexOf(0) !== -1) {
+          return "BEEP!";
+        } else {
+          for (var index = topNumber; index>-1; index--) {
+            array.push(index  );
+          };
+
+          return array;
+        }
       }
     };
 
