1c9ef743a3b0a2f5f14e235f0cddb1fd12218eda
Jimmy McNamara <jmcnamara44@gmail.com>
2018-03-30 12:34:11 -0700

    return 'beep' and 'boop' when user enters a number containing a '0' or a '1' respectively.  convert userinput to a string in js to find place in string. passes sixth and seventh listed behaviors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
-var message = function(input) {
-  if (input % 3 === 0 && input !== 0) {
-    return "I'm sorry Dave. I'm afraid I can't do that";
-  } else if (input === 1) {
-    return "BOOP!";
-  } else if (input === 0) {
-    return "BEEP!"
-  } else {
-    return (input - input) + "-" + input;
-  }
-};
-
-
+//business logic
+//user-interface logic
 
 
 
 $(document).ready(function() {
-  $("#number-input").submit(function(event) {
+  $("form#number-input").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#number").val());
     var result = message(userInput);
+
+    function message (input) {
+
+      var string = input.toString();
+      if (input % 3 === 0 && input !== 0) {
+        return "I'm sorry Dave. I'm afraid I can't do that";
+      } else if (string.indexOf(1) !== -1) {
+        return "BOOP!";
+      } else if (string.indexOf(0) !== -1) {
+        return "BEEP!";
+      } else {
+        return (input - input) + "-" + input;
+      }
+
+alert(string.length);
+    };
 
     $("#results").text(result);
   });
