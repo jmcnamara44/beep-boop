259f58c624d24fcd72b98ed6a0882f5758237321
Jimmy McNamara <jmcnamara44@gmail.com>
2018-04-08 17:05:55 -0700

    clean up the js file.  business and user logic are now separate, and uneeded variable names have been removed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,44 @@
 //business logic
-//user-interface logic
 
+var userInput;
+var result;
+var userName;
+var array = [];
+
+function message (input) {
+
+  array = [];
+  $("#results").hide();
+
+  if (input % 1 === 0 && userName) {
+    if (input % 3 === 0 && input !== 0) {
+      return "I'm sorry " + userName + ". I'm afraid I can't do that.";
+    } else if (input.toString().indexOf(1) !== -1) {
+      return "BOOP!";
+    } else if (input.toString().indexOf(0) !== -1) {
+      return "BEEP!";
+    } else {
+      for (var index = input; index>-1; index--) {
+        array.push(index);
+      };
+    return array.reverse();
+    }
+  } else {
+    return "Stop fooling around " + userName +".";
+    }
+};
+
+//user-interface logic
 
 $(document).ready(function() {
   $("#number-input").submit(function(event) {
 
     event.preventDefault();
 
-    var userInput = parseInt($("#number").val());
-    var result = message(userInput);
-    var userName;
-    var array = [];
-    var topNumber;
-    var string;
+    userInput = parseInt($("#number").val());
+    userName = $("#name").val();
+    result = message(userInput);
 
-    function message (input) {
-      userName = $("#name").val();
-      array = [];
-      topNumber = input;
-      string = input.toString();
-      $("#results").hide();
-
-      if (input % 1 === 0 && userName) {
-        if (input % 3 === 0 && input !== 0) {
-          return "I'm sorry " + userName + ". I'm afraid I can't do that.";
-        } else if (string.indexOf(1) !== -1) {
-          return "BOOP!";
-        } else if (string.indexOf(0) !== -1) {
-          return "BEEP!";
-        } else {
-          for (var index = topNumber; index>-1; index--) {
-            array.push(index  );
-          };
-
-        return array.reverse();
-        }
-      } else {
-        return "Stop fooling around " + userName +".";
-        }
-
-    };
     $("#results").fadeIn(1000);
     $("#results").text(result);
   });
